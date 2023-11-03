32b58ed07974bacd8ce5551e56fc9a7156d2b505
yahya bnoun <yahyaiko22@gmail.com>
2023-11-03 23:03:33 +0100

    Routing logic

--- app1/urls.py
+++ app1/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index),
+    # path("sports", views.sports_view),
+    # path("finance", views.finance_view),
+    path("<topic>", views.news_view),
+    path("<int:num1>/<int:num2>", views.sum_view),
+
 ]
--- app1/views.py
+++ app1/views.py
@@ -4,5 +4,32 @@ from django.http import HttpResponse
 
 
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+# ----------------------------------------------------------------------------------------------
+
+# def sports_view(request):
+#     return HttpResponse("sports page")
+
+# def finance_view(request):
+#     return HttpResponse("finance page")
+
+# ----------------------------------------------------------------------------------------------
+
+articles = {
+    'sports':"sports page",
+    'finance':"finance page",
+    'politics':"politics page",
+}
+
+
+def news_view(request,topic):
+    return HttpResponse(articles[topic])
+
+# ----------------------------------------------------------------------------------------------
+
+def sum_view(request,num1,num2):
+    result = num1 + num2
+    return HttpResponse(str(result))
